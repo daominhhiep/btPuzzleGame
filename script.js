f8153d57f6349fec12eebc4947ebd3a7287ef464
Hiệp Đào Minh <daominhhiep97@gmail.com>
2020-06-12 09:14:05 +0700

    puzzle game>

---
+++ script.js
@@ -0,0 +1,81 @@
+function changeImage1() {
+    let img1 = document.getElementById('pic1');
+    if (img1.src.match('cat')) {
+        img1.src = 'image/monkey1.jpg';
+        img1.value = "monkey";
+    } else if (img1.src.match('monkey')) {
+        img1.src = 'image/panda1.jpg';
+        img1.value = "panda";
+    } else if (img1.src.match('panda')) {
+        img1.src = 'image/panther1.jpg';
+        img1.value = "panther";
+    } else {
+        img1.src = 'image/cat1.jpg';
+        img1.value = "cat";
+    }
+
+
+}
+function changeImage2() {
+    let img2 = document.getElementById('pic2');
+    if (img2.src.match('cat')) {
+        img2.src = 'image/monkey2.jpg';
+        img2.value = "monkey";
+    } else if (img2.src.match('monkey')) {
+        img2.src = 'image/panda2.jpg';
+        img2.value = "panda";
+    } else if (img2.src.match('panda')) {
+        img2.src = 'image/panther2.jpg';
+        img2.value = "panther";
+    } else {
+        img2.src = 'image/cat2.jpg';
+        img2.value = "cat";
+    }
+}
+function changeImage3() {
+    let img3 = document.getElementById('pic3');
+    if (img3.src.match('cat')) {
+        img3.src = 'image/monkey3.jpg';
+        img3.value = "monkey";
+    } else if (img3.src.match('monkey')) {
+        img3.src = 'image/panda3.jpg';
+        img3.value = "panda";
+    } else if (img3.src.match('panda')) {
+        img3.src = 'image/panther3.jpg';
+        img3.value = "panther";
+    } else {
+        img3.src = 'image/cat3.jpg';
+        img3.value = "cat";
+    }
+}
+function changeImage4() {
+    let img4 = document.getElementById('pic4');
+    if (img4.src.match('cat')) {
+        img4.src = 'image/monkey4.jpg';
+        img4.value = "monkey";
+    } else if (img4.src.match('monkey')) {
+        img4.src = 'image/panda4.jpg';
+        img4.value = "panda";
+    } else if (img4.src.match('panda')) {
+        img4.src = 'image/panther4.jpg';
+        img4.value = "panther";
+    } else {
+        img4.src = 'image/cat4.jpg';
+        img4.value = "cat";
+    }
+}
+function changeImage5() {
+    let img5 = document.getElementById('pic5');
+    if (img5.src.match('cat')) {
+        img5.src = 'image/monkey5.jpg';
+        img5.value = "monkey";
+    } else if (img5.src.match('monkey')) {
+        img5.src = 'image/panda5.jpg';
+        img5.value = "panda";
+    } else if (img5.src.match('panda')) {
+        img5.src = 'image/panther5.jpg';
+        img5.value = "panther";
+    } else {
+        img5.src = 'image/cat5.jpg';
+        img5.value = "cat";
+    }}
